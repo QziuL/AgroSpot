92984af6f8e0302112c58b9f629c0cbc18ae2a14
QziuL <lf.quinholi2@gmail.com>
2024-09-08 11:50:33 -0300

    first commit

---
+++ src/main/java/agrospot/AgrospotApplication.java
@@ -0,0 +1,13 @@
+package agrospot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrospotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrospotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agrospot/AgrospotApplicationTests.java
@@ -0,0 +1,13 @@
+package agrospot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrospotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
