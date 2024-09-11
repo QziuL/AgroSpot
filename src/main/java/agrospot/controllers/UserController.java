daa8139d6e741c3ab2052d925bb35e251482f1af
QziuL <lf.quinholi2@gmail.com>
2024-09-10 21:28:14 -0300

    feat: user controller

---
+++ src/main/java/agrospot/controllers/UserController.java
@@ -0,0 +1,33 @@
+package agrospot.controllers;
+
+import agrospot.dtos.request.UserDTO;
+import agrospot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+//    @GetMapping
+//    public ResponseEntity<List<UserDTO>> getAllUsers() {
+//        return ResponseEntity.ok(userService.findAllUsers());
+//    }
+
+    @PostMapping
+    public ResponseEntity<Object> createNewUser(@RequestBody UserDTO userDTO) {
+        return (userService.createUser(userDTO))
+                ? ResponseEntity.status(HttpStatus.CREATED).body("Created successfully!")
+                : ResponseEntity.badRequest().build();
+
+    }
+}
