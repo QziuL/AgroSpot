b173ddf046661aabb4a9c3d7a2bb403a5ebd0da6
QziuL <lf.quinholi2@gmail.com>
2024-09-19 18:59:17 -0300

    feat: BCrypt for user password

--- src/main/java/agrospot/dtos/request/UserDTO.java
+++ src/main/java/agrospot/dtos/request/UserDTO.java
@@ -1,20 +1,21 @@
 package agrospot.dtos.request;
 
 import agrospot.models.UserModel;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public record UserDTO(String name, String email, String password) {
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public UserDTO(String name, String email, String password) {
         this.name = name;
         this.email = email;
-        this.password = password;
+        this.password = encoder.encode(password);
     }
 
     public UserModel convertDtoToModel(UserDTO userDTO) {
-        List<String> roles = new ArrayList<>();
-        roles.add("BASIC");
-        return new UserModel(this.name, this.email, this.password, roles);
+        return new UserModel(this.name, this.email, this.password);
     }
 }
