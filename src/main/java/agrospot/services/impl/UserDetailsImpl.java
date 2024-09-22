7d6ef8a47270b5121e66cd278b33ebfc25e6e78d
QziuL <lf.quinholi2@gmail.com>
2024-09-22 18:41:51 -0300

    feat: class for implementation of UserDetails

---
+++ src/main/java/agrospot/services/impl/UserDetailsImpl.java
@@ -0,0 +1,32 @@
+package agrospot.services.impl;
+
+import agrospot.models.UserModel;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+    private final UserModel user;
+
+    public UserDetailsImpl(UserModel user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+}
