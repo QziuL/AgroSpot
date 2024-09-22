970e19618a54f493cd20b1e27701d9e6a186f802
QziuL <lf.quinholi2@gmail.com>
2024-09-22 18:45:57 -0300

    refactor: roles enum

--- src/main/java/agrospot/enums/RolesEnum.java
+++ src/main/java/agrospot/enums/RolesEnum.java
@@ -1,7 +1,17 @@
 package agrospot.enums;
 
 public enum RolesEnum {
-    USER,
-    ADMIN,
-    PRODUCER
+    USER("user"),
+    ADMIN("admin"),
+    PRODUCER("producer");
+
+    private final String role;
+
+    RolesEnum(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
