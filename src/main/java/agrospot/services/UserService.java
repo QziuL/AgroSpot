515d73ab736a3db3a542d0105dda1109cccee955
QziuL <lf.quinholi2@gmail.com>
2024-10-14 11:14:21 -0300

    refactor: response type now is a dto for user list

--- src/main/java/agrospot/services/UserService.java
+++ src/main/java/agrospot/services/UserService.java
@@ -1,13 +1,14 @@
 package agrospot.services;
 
 import agrospot.dtos.request.CreateUserDTO;
+import agrospot.dtos.response.ListUserDTO;
 
 import java.util.List;
 
 public interface UserService {
     Boolean createUser(CreateUserDTO userDto);
     CreateUserDTO findUserByEmail(String email);
-    List<CreateUserDTO> findAllUsers();
+    List<ListUserDTO> findAllUsers();
     CreateUserDTO updateUserByEmail(String email);
     Boolean deleteUserById(String id);
 }
