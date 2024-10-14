d36ee9a89e3202734b0160673923212cec9323fb
QziuL <lf.quinholi2@gmail.com>
2024-10-14 11:05:22 -0300

    feat: controller for admin with home method

---
+++ src/main/java/agrospot/controllers/AdminController.java
@@ -0,0 +1,14 @@
+package agrospot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping
+    public String home(){
+        return "Hello Admin!";
+    }
+}
