4293a234a98d425919a320e5992bc08e92dcd23a
QziuL <lf.quinholi2@gmail.com>
2024-10-20 14:43:51 -0300

    refactor: substituted dependency injection for notation Autowired and removed comments

--- src/main/java/agrospot/controllers/UserController.java
+++ src/main/java/agrospot/controllers/UserController.java
@@ -1,9 +1,8 @@
 package agrospot.controllers;
 
-import agrospot.dtos.request.CreateUserDTO;
 import agrospot.dtos.response.ListUserDTO;
 import agrospot.services.UserService;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +11,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
     UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping
     public ResponseEntity<List<ListUserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.findAllUsers());
     }
-
-//    @PostMapping
-//    public ResponseEntity<Object> createNewUser(@RequestBody CreateUserDTO createUserDTO) {
-//        return (userService.createUser(createUserDTO))
-//                ? ResponseEntity.status(HttpStatus.CREATED).body("User created successfully.")
-//                : ResponseEntity.badRequest().build();
-//
-//    }
 }
