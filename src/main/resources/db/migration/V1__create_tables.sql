ed2ff7b94c3c05ef0e323d3eb6cbc7afbf9d0fff
QziuL <lf.quinholi2@gmail.com>
2024-10-20 17:25:07 -0300

    refactor: update columns name

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    external_id     UUID NOT NULL,
+    externalId      UUID NOT NULL,
     name            VARCHAR(255) NOT NULL,
     email           VARCHAR(255) NOT NULL UNIQUE,
     password        VARCHAR(255) NOT NULL
@@ -13,15 +13,15 @@ CREATE TABLE roles(
 
 CREATE TABLE users_roles(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id         BIGINT NOT NULL,
-    role_id         BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id)
+    userId          BIGINT NOT NULL,
+    roleId          BIGINT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users (id),
+    FOREIGN KEY (roleId) REFERENCES roles (id)
 );
 
 CREATE TABLE addresses(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    zip_code        VARCHAR(8),
+    zipCode         VARCHAR(8),
     street          VARCHAR(255),
     neighborhood    VARCHAR(255),
     city            VARCHAR(255)
@@ -29,20 +29,20 @@ CREATE TABLE addresses(
 
 CREATE TABLE producers(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id         BIGINT NOT NULL,
-    address_id      BIGINT NOT NULL,
-    external_id     UUID NOT NULL,
+    userId          BIGINT NOT NULL,
+    addressId       BIGINT NOT NULL,
+    externalId      UUID NOT NULL,
     cpf             VARCHAR(11) NOT NULL UNIQUE,
     phone           VARCHAR(11) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (address_id) REFERENCES addresses (id)
+    FOREIGN KEY (userId) REFERENCES users (id),
+    FOREIGN KEY (addressId) REFERENCES addresses (id)
 );
 
 CREATE TABLE products(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name            VARCHAR(255),
     description     TEXT,
-    external_id     UUID NOT NULL,
+    externalId      UUID NOT NULL,
     pathImage       VARCHAR(255)
 );
 
@@ -61,28 +61,28 @@ CREATE TABLE products(
 
 CREATE TABLE product_producer(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    product_id      BIGINT NOT NULL,
-    producer_id     BIGINT NOT NULL,
+    productId      BIGINT NOT NULL,
+    producerId     BIGINT NOT NULL,
     available       BOOLEAN NOT NULL,
     price           NUMERIC(6,2) NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (id),
-    FOREIGN KEY (producer_id) REFERENCES producers (id)
+    FOREIGN KEY (productId) REFERENCES products (id),
+    FOREIGN KEY (producerId) REFERENCES producers (id)
 );
 
 CREATE TABLE fairs(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    address_id      BIGINT NOT NULL,
-    external_id     UUID NOT NULL,
+    addressId       BIGINT NOT NULL,
+    externalId      UUID NOT NULL,
     name            VARCHAR(255),
     description     TEXT,
-    FOREIGN KEY (address_id) REFERENCES addresses (id)
+    FOREIGN KEY (addressId) REFERENCES addresses (id)
 );
 
 CREATE TABLE producer_fair(
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    producer_id     BIGINT NOT NULL,
-    fair_id    BIGINT NOT NULL,
-    FOREIGN KEY (producer_id) REFERENCES producers (id),
-    FOREIGN KEY (fair_id) REFERENCES fairs (id)
+    producerId      BIGINT NOT NULL,
+    fairId          BIGINT NOT NULL,
+    FOREIGN KEY (producerId) REFERENCES producers (id),
+    FOREIGN KEY (fairId) REFERENCES fairs (id)
 );
 
