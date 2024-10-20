176e9e68289ecf933adbf62781b8e21bc089caa9
QziuL <lf.quinholi2@gmail.com>
2024-10-20 17:27:45 -0300

    feat: query for find user by external id (uuid)

--- src/main/java/agrospot/repositorys/UserRepository.java
+++ src/main/java/agrospot/repositorys/UserRepository.java
@@ -2,11 +2,19 @@ package agrospot.repositorys;
 
 import agrospot.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     Optional<UserModel> findByEmail(String email);
+//    @Query(value = "select UserModel from UserModel where UserModel.externalId=externalId")
+    //Optional<UserModel> findByExternalId(UUID externalId);
+
+    @Query("SELECT u FROM UserModel u WHERE u.externalId = :externalId")
+    Optional<UserModel> findByExternalId(@Param("externalId") UUID externalId);
 }
