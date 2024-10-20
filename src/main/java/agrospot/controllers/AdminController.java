d05f25965cfda3a6e8034dd4bc7bab8d827332e9
QziuL <lf.quinholi2@gmail.com>
2024-10-20 19:04:52 -0300

    feat: methods create, get all, get one and delete for Product feat: method get one User by email

--- src/main/java/agrospot/controllers/AdminController.java
+++ src/main/java/agrospot/controllers/AdminController.java
@@ -1,7 +1,10 @@
 package agrospot.controllers;
 
 import agrospot.dtos.request.CreateUserDTO;
+import agrospot.dtos.request.ProductRequestDTO;
 import agrospot.dtos.response.ListUserDTO;
+import agrospot.dtos.response.ProductResponseDTO;
+import agrospot.services.ProductService;
 import agrospot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,16 +19,32 @@ import java.util.UUID;
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
-    UserService userService;
+    private UserService userService;
+    @Autowired
+    private ProductService productService;
 
     @GetMapping
     public String home(){
         return "Hello Admin!";
     }
 
+
+    /*
+        ====================================
+        == Métodos relacionados a User ==
+        ====================================
+    */
     @GetMapping("/user")
     public ResponseEntity<List<ListUserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/user/{email}")
+    public ResponseEntity<ListUserDTO> getUserByEmail(@PathVariable String email) {
+        ListUserDTO userDto = userService.findUserByEmail(email);
+        if(!Objects.isNull(userDto))
+            return ResponseEntity.ok(userDto);
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/user")
@@ -36,18 +55,42 @@ public class AdminController {
 
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<ListUserDTO> getUserByEmail(@RequestParam("email") String email) {
-        ListUserDTO userDto = userService.findUserByEmail(email);
-        if(!Objects.isNull(userDto))
-            return ResponseEntity.ok(userDto);
-        return ResponseEntity.notFound().build();
-    }
-
     @DeleteMapping("/user/{externalId}")
     public ResponseEntity<Object> deleteUser(@PathVariable UUID externalId) {
         return (userService.deleteUserByExternalId(externalId))
                 ? ResponseEntity.ok().build()
                 : ResponseEntity.notFound().build();
+    }
+
+    /*
+        ====================================
+        == Métodos relacionados a Produto ==
+        ====================================
+    */
+    @PostMapping("/product")
+    public ResponseEntity<Object> createNewProduct(@RequestBody ProductRequestDTO productDto){
+        return (productService.createProduct(productDto))
+                ? ResponseEntity.status(HttpStatus.CREATED).body("Product created successfully.")
+                : ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<List<ProductResponseDTO>> getAllProduct() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/product/{externalId}")
+    public ResponseEntity<ProductResponseDTO> getProductByExternalId(@PathVariable UUID externalId) {
+        ProductResponseDTO productDto = productService.getProductByExternalId(externalId);
+        if(!Objects.isNull(productDto))
+            return ResponseEntity.ok(productDto);
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/product/{externalId}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable UUID externalId) {
+        return (productService.deleteProductByExternalId(externalId))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.badRequest().build();
     }
 }
