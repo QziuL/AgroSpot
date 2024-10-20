585a4e9507bf609de7a9b340d1530adfc8c54286
QziuL <lf.quinholi2@gmail.com>
2024-10-20 17:30:47 -0300

    refactor: not return password, return external id (uuid)

--- src/main/java/agrospot/dtos/response/ListUserDTO.java
+++ src/main/java/agrospot/dtos/response/ListUserDTO.java
@@ -2,5 +2,5 @@ package agrospot.dtos.response;
 
 import java.util.List;
 
-public record ListUserDTO(String name, String email, String password, List<String> roles) {
+public record ListUserDTO(String name, String email, String external_id, List<String> roles) {
 }
